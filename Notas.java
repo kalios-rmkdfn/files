a82586a7edbae908d6247cb08d8943b1155dbd14
Kalios_RMKDFN <14222398+kalios-rmkdfn@users.noreply.github.com>
2024-01-13 19:07:00 -0600

    Update Notas.java

--- Notas.java
+++ Notas.java
@@ -1,2 +1,93 @@
-<h1> Ok. Esto es una prueba. </h1>
-Una segunda
+package com.servlet.modelo.entity;
+
+import java.time.LocalDate;
+
+public class Manual {
+	private int id;
+    private String titulo;
+    private String categoria;
+    private LocalDate fecha;
+    private String ubicacion;
+    private String imagen;
+    private String resumen;
+    private String articulo;
+
+    // Constructor
+    public Manual(int id, String titulo, String categoria, LocalDate fecha, String ubicacion,
+                  String imagen, String resumen, String articulo) {
+        this.id = id;
+        this.titulo = titulo;
+        this.categoria = categoria;
+        this.fecha = fecha;
+        this.ubicacion = ubicacion;
+        this.imagen = imagen;
+        this.resumen = resumen;
+        this.articulo = articulo;
+    }
+
+    // Getters y Setters para los atributos
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getResumen() {
+        return resumen;
+    }
+
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+    public String getArticulo() {
+        return articulo;
+    }
+
+    public void setArticulo(String articulo) {
+        this.articulo = articulo;
+    }
+}
